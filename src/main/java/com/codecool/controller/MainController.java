5b331d9300c37d716ba7deb1c923f4b3fb99b7da
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-13 22:29:01 +0200

    removed other model directory

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,6 +1,6 @@
 package com.codecool.controller;
 
-import com.codecool.model.forms.ConsumptionFormObject;
+import com.codecool.models.forms.ConsumptionFormObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/codecool/models/forms/ConsumptionFormObject.java
+++ src/main/java/com/codecool/models/forms/ConsumptionFormObject.java
@@ -1,4 +1,4 @@
-package com.codecool.model.forms;
+package com.codecool.models.forms;
 
 import lombok.Data;
 
