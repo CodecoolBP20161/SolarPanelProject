9e453c02c6fe8c7537e344914c7416d4f19bbafd
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-14 09:58:07 +0200

    event handlers for form two(in 'ajánlat') are added

--- src/main/resources/static/assets/js/offer-handler.js
+++ src/main/resources/static/assets/js/offer-handler.js
@@ -1,8 +1,51 @@
 $(document).ready(function() {
-        var metricSelect = $('#stepOneSelect');
-        metricSelect.on('change', function () {
-            $('#metricHeader').html(metricSelect.find('option:selected').val());
+
+    // Event Listeners for form one( annual electric consumption or monthly electric bill selection)
+
+    var metricSelect = $('#stepOneSelect');
+
+    metricSelect.on('change', function () {
+        $('#metricHeader').html(metricSelect.find('option:selected').val());
+    });
+
+    // Event Listeners for form two (Solar panel and inverter selection)
+
+    var inverterInput = $('#inverterInput');
+    var panelInput = $('#panelInput');
+    inverterInput.val('initial');
+    panelInput.val('initial');
+    var formSubmitbutton = $('#submitPanelAndInverter');
+    var selectedInverter = null;
+    var selectedPanel = null;
+
+    // Toggles class on clicked inverter and set hidden input's value to clicked inverters id
+    $('.inverter').on('click', function (event) {
+        if (selectedInverter){
+            selectedInverter.toggleClass('active');
+        }
+        selectedInverter = $(event.target.closest('div[class^="testimonial-block cyan-background inverter"]'));
+        selectedInverter.toggleClass('active');
+        inverterInput.val(selectedInverter.attr('data')).change();
+    });
+
+    // Toggles class on clicked panel and set hidden input's value to clicked panel id
+    $('.panel').on('click', function (event) {
+
+        if (selectedPanel){
+            selectedPanel.toggleClass('active');
+        }
+        selectedPanel = $(event.target.closest('div[class^="testimonial-block cyan-background panel"]'));
+        selectedPanel.toggleClass('active');
+        panelInput.val(selectedPanel.attr('data')).change()
+    });
+
+    // When an input is changed, it checks whether there are still input with initial value,
+    // and sets the buttons disabled property accordingly
+    inverterInput.add(panelInput).on('change', function () {
+        if (inverterInput.val() !== 'initial' && panelInput.val() !== 'initial')
+            formSubmitbutton.prop('disabled', false);
         })
-    }
+
+}
 );
 
