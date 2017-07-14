bf9dee963c0ac9ab9f47328e4c18c6c7a81333c5
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-14 09:59:17 +0200

    Get and Post route is configured for '/ajanlat/2'

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.codecool.controller;
 
-import com.codecool.models.forms.ConsumptionFormObject;
+import com.codecool.models.forms.ConsumptionForm;
+import com.codecool.models.forms.PanelAndInverterForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -22,20 +24,27 @@ public class MainController {
 
     @GetMapping("/ajanlat/1")
     public String getOfferStep1(Model model){
-        model.addAttribute("consumption", new ConsumptionFormObject());
+        model.addAttribute("consumption", new ConsumptionForm());
         model.addAttribute("step", '1');
         return "offer";
     }
     @PostMapping("/ajanlat/1")
-    public String postOfferStep1(Model model, @ModelAttribute ConsumptionFormObject consumption){
+    public String postOfferStep1(Model model, @ModelAttribute ConsumptionForm consumption){
         log.info("Consumption: " + consumption.getValue() + " " + consumption.getMetric());
-        model.addAttribute("step", '2');
-        return "offer";
+        return "redirect:/ajanlat/2";
     }
 
     @GetMapping("/ajanlat/2")
     public String getOfferStep2(Model model){
+        model.addAttribute("deviceForm", new PanelAndInverterForm());
         model.addAttribute("step", '2');
+        return "offer";
+    }
+
+    @PostMapping("/ajanlat/2")
+    public String postOfferStep2(Model model, @ModelAttribute PanelAndInverterForm device){
+        log.info("Consumption: " + device.getInverterId() + " " + device.getPanelId());
+        model.addAttribute("step", '3');
         return "offer";
     }
 
