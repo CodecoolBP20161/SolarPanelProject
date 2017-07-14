c35cfca121a9d5627d2a1e51230aba7f155cc419
gamte19 <gamte19@gmail.com>
2017-07-14 15:06:41 +0200

    Extend modell

--- src/main/java/com/codecool/models/Inverter.java
+++ src/main/java/com/codecool/models/Inverter.java
@@ -20,18 +20,33 @@ public class Inverter {
     @NotEmpty
     private String name;
 
+    @NotEmpty
+    private String brand;
+
+    private Integer wifiModule;
+
+    @NotEmpty
+    private Integer phase;
+
     @NotNull
     private int price;
+
+    @NotEmpty
+    private Double vatt;
 
     @NotNull
     private int capacity;
 
-
     private String description;
 
-    public Inverter(String name, int price, int capacity, String description) {
+
+    public Inverter(String name, String brand, Integer wifiModule, Integer phase, int price, Double vatt, int capacity, String description) {
         this.setName(name);
+        this.setBrand(brand);
+        this.setWifiModule(wifiModule);
+        this.setPhase(phase);
         this.setPrice(price);
+        this.setVatt(vatt);
         this.setCapacity(capacity);
         this.setDescription(description);
     }
