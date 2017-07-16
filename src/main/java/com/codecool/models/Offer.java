cffa60ea143abdeb2f0d76f8dca9f3843b8220fe
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-16 13:34:03 +0200

    implemented  removeLineItem

--- src/main/java/com/codecool/models/Offer.java
+++ src/main/java/com/codecool/models/Offer.java
@@ -9,17 +9,28 @@ import java.util.ArrayList;
 
 @Data
 public class Offer {
-    private String name;
+    private static long idCount = 1;
     private long id;
     private double taxRate;
     private ArrayList<LineItem> lineItems;
     private boolean isNetworkUpgradeNeeded;
+
+    public Offer( double taxRate){
+        id = idCount++;
+        lineItems = new ArrayList<>();
+        isNetworkUpgradeNeeded = false;
+    }
+
     @Setter(AccessLevel.NONE)
     private BigDecimal nettoTotalPrice;
 
     public void addLineItem(LineItem lineItem) {
         lineItems.add(lineItem);
         nettoTotalPrice = nettoTotalPrice.add(lineItem.getTotal());
+    }
+    public void removeLineItem(LineItem lineItem){
+        lineItems.remove(lineItem);
+        nettoTotalPrice = nettoTotalPrice.subtract(lineItem.getTotal());
     }
 }
 
