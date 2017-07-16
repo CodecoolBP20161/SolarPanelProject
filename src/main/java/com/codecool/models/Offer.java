a4a5435cb0997623d91ca62991aa088b3d963e41
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-16 13:13:51 +0200

    created class t osend to Pdf generator service

---
+++ src/main/java/com/codecool/models/Offer.java
@@ -0,0 +1,26 @@
+package com.codecool.models;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+@Data
+public class Offer {
+    private String name;
+    private long id;
+    private double taxRate;
+    private ArrayList<LineItem> lineItems;
+    private boolean isNetworkUpgradeNeeded;
+    @Setter(AccessLevel.NONE)
+    private BigDecimal nettoTotalPrice;
+
+    public void addLineItem(LineItem lineItem) {
+        lineItems.add(lineItem);
+        nettoTotalPrice.add(lineItem.getTotal());
+    }
+}
+
+
