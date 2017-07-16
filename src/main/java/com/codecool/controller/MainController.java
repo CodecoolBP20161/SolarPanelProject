983ee98f6c162565aa341d429090417d80b19bd2
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-16 14:53:48 +0200

    d

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.codecool.controller;
 
 import com.codecool.models.Inverter;
+import com.codecool.models.Offer;
 import com.codecool.models.SolarPanel;
 import com.codecool.models.forms.ConsumptionForm;
-import com.codecool.models.forms.PanelAndInverterForm;
+import com.codecool.models.forms.EmailForm;
+import com.codecool.models.forms.DeviceForm;
 import com.codecool.repositories.InverterRepository;
 import com.codecool.repositories.SolarPanelRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -13,23 +15,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
 @Controller
+@SessionAttributes({"consumption", "deviceForm", "email", "offer"})
 public class MainController {
 
     private SolarPanelRepository solarPanelRepository;
     private InverterRepository inverterRepository;
+    private final String CONSUMPTION = "consumption";
+    private final String DEVICE = "deviceForm";
+    private final String EMAIL = "email";
+    private final String STEP = "step";
+    private final String offer = "offer";
 
     @Autowired
     public MainController(InverterRepository inverterRepository, SolarPanelRepository solarPanelRepository){
         this.solarPanelRepository = solarPanelRepository;
         this.inverterRepository = inverterRepository;
     }
-
-
 
     @GetMapping("/")
     public String getIndex(){
@@ -42,47 +50,62 @@ public class MainController {
     }
 
     @GetMapping("/ajanlat/1")
-    public String getOfferStep1(Model model){
-        ConsumptionForm consumptionForm = new ConsumptionForm();
-        consumptionForm.setPhase(1);
-        model.addAttribute("consumption", consumptionForm);
-        model.addAttribute("step", '1');
+    public String getOfferStep1(Model model, HttpSession session){
+        ConsumptionForm consumptionForm = session.getAttribute(CONSUMPTION) == null ?
+            new ConsumptionForm() : (ConsumptionForm) session.getAttribute(CONSUMPTION);
+        model.addAttribute(CONSUMPTION, consumptionForm);
+        model.addAttribute(STEP, '1');
         return "offer";
     }
     @PostMapping("/ajanlat/1")
-    public String postOfferStep1(Model model, @ModelAttribute ConsumptionForm consumption){
-        log.info("Consumption: " + consumption.getValue() + " " + consumption.getMetric() + "\n Number of phases: " + consumption.getPhase());
+    public String postOfferStep1(@ModelAttribute ConsumptionForm consumption, HttpSession session){
+        session.setAttribute(CONSUMPTION, consumption);
+        log.info("Consumption: " + consumption.getValue() + consumption.getMetric() + " No.Phase: " + consumption.getPhase());
         return "redirect:/ajanlat/2";
     }
 
     @GetMapping("/ajanlat/2")
-    public String getOfferStep2(Model model){
+    public String getOfferStep2(Model model, HttpSession session){
         List<SolarPanel> solarPanelList = solarPanelRepository.findAll();
         List<Inverter> inverterList = inverterRepository.findAll();
-        model.addAttribute("deviceForm", new PanelAndInverterForm());
+
+        DeviceForm pAndIForm = session.getAttribute(DEVICE) == null ?
+                new DeviceForm() : (DeviceForm) session.getAttribute(DEVICE);
+
+        model.addAttribute(DEVICE, pAndIForm);
         model.addAttribute("solarPanelList", solarPanelList);
         model.addAttribute("inverterList", inverterList);
-        model.addAttribute("step", '2');
+        model.addAttribute(STEP, '2');
         return "offer";
     }
 
     @PostMapping("/ajanlat/2")
-    public String postOfferStep2(Model model, @ModelAttribute PanelAndInverterForm device){
+    public String postOfferStep2(@ModelAttribute DeviceForm device, HttpSession session){
+        session.setAttribute(DEVICE, device);
         log.info("Devices: " + device.getInverterId() + " " + device.getPanelId());
-        model.addAttribute("step", '3');
-        return "offer";
+        return "redirect:/ajanlat/3";
     }
 
 
     @GetMapping("/ajanlat/3")
-    public String getOfferStep3(Model model){
-        model.addAttribute("step", '3');
+    public String getOfferStep3(Model model, HttpSession session){
+        EmailForm email = session.getAttribute(EMAIL) == null ?
+                new EmailForm() : (EmailForm) session.getAttribute(EMAIL);
+
+        model.addAttribute("email", email);
+        model.addAttribute(STEP, '3');
+        return "offer";
+    }
+    @PostMapping("/ajanlat/3")
+    public String getOfferStep3(Model model, @ModelAttribute EmailForm email, HttpSession session){
+        session.setAttribute(EMAIL, email);
+        log.info("Entered email: ", email.getEmail());
         return "offer";
     }
 
     @GetMapping("/ajanlat/4")
     public String getOfferStep4(Model model){
-        model.addAttribute("step", '4');
+        model.addAttribute(STEP, '4');
         return "offer";
     }
 
