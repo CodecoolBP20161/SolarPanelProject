a0da4fb904fc05bd5e70a6fb4704ce35f60be0e7
gamte19 <gamte19@gmail.com>
2017-07-19 23:01:31 +0200

    Add custom find method

--- src/main/java/com/codecool/repositories/InverterRepository.java
+++ src/main/java/com/codecool/repositories/InverterRepository.java
@@ -2,8 +2,18 @@ package com.codecool.repositories;
 
 import com.codecool.models.Inverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InverterRepository extends JpaRepository<Inverter, Integer> {
+
+    @Query(value =
+            "SELECT inv FROM Inverter inv  " +
+            "WHERE inv.capacity >= :capacity AND inv.capacity <= (:capacity + 999) " +
+            "ORDER BY inv.price ASC")
+    List<Inverter> findByCustomerValue(@Param("capacity") Integer capacity);
 }
