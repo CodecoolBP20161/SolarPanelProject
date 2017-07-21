b5c27b631b5cf6af60b82c59edaa317ad9146945
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-21 16:07:19 +0200

    Problem solved

--- src/main/resources/static/assets/js/offer-handler.js
+++ src/main/resources/static/assets/js/offer-handler.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         .add(phaseOneInput)
         .add(phaseTwoInput).on('change paste keyup', function () {
         // If phase is 1, and the selected metric is kWh and the inputs value is bigger than 5, then the message should show
-        if (phaseOneInput.is(':checked') && metricSelect.find('option:selected').val() == 'kWh' && metricInput.val() >= 5){
+        if (phaseOneInput.is(':checked') && metricSelect.find('option:selected').val() == 'kWh' && metricInput.val() >= 5) {
             alertSpan.prop('hidden', false);
         }
         //TODO: write condition for (havi villanyszámla alapján)
@@ -78,8 +78,112 @@ $(document).ready(function () {
 
 //_______________________________________________________________________________________________
 
-})
-;
+
+    // FORM 3
+    $('#submitEmail').on('click', function (event) {
+        event.preventDefault();
+        {
+            $.ajax({
+                url: "http://52.15.84.238:1350/api/printpdf1",
+                type: 'POST',
+                async: true,
+                contentType: "application/json",
+                data: JSON.stringify(
+                    {
+                        "id": 123321,
+
+                        "items" :
+                            [
+                                {
+                                    "name": "Termék1",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Fasza3",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Termék4",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Termék5",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                }
+                            ],
+
+                        "services" :
+                            [
+                                {
+                                    "name": "Service1",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Service2",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Service3",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Service4",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                },
+                                {
+                                    "name": "Service5",
+                                    "price": 10000,
+                                    "quantity": 10,
+                                    "subtotal": 100000,
+                                    "description": "Legjobb termék"
+                                }
+
+                            ],
+
+                        "tax_rate": 1.27,
+
+                        "total": 9999999
+
+                    }
+                ),
+                success: function (pdf) {
+                    var w = window.open('about:blank', 'Offer');
+                    w.document.write(pdf);
+                    w.document.close();
+                    var w2 = window.open("data:application/pdf," + encodeURI(pdf));
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status);
+                    alert(thrownError);
+                }
+            });
+        }
+    });
+});
 
 
 
