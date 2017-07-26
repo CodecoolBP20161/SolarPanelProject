a9aec4976c30cb2bc98cd62416142d5d0abe4073
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-26 17:21:08 +0200

    PDf new template 0.1

--- src/main/PdfServer/routes.js
+++ src/main/PdfServer/routes.js
@@ -5,16 +5,16 @@ var path = require("path");
 var fs = require('fs');
 var templateRoute = path.resolve(__dirname, 'templates/');
 var cssRoute = path.resolve(__dirname, 'templates/assets/css/');
-var offer_template = 'offer_template.html';
-var offer_template_v1 = 'offer_template_v1.html';
+var offer_template = '/offer_template.html';
+var offer_template_v1 = '/offer_template_v1.html';
 
 // Style files loaded
-var bootstrap = fs.readFileSync(cssRoute + 'bootstrap.min.css' , 'utf-8');
-var flatIcon = fs.readFileSync(cssRoute + 'flaticon.css' , 'utf-8');
-var fontAwesome = fs.readFileSync(cssRoute + 'font-awesome.css' , 'utf-8');
-var style = fs.readFileSync(cssRoute + 'style.css' , 'utf-8');
-var revStyle = fs.readFileSync(cssRoute + 'rev-style.css' , 'utf-8');
-var themeColor = fs.readFileSync(cssRoute + 'theme-color/bootstrap.css' , 'utf-8');
+var bootstrap = fs.readFileSync(cssRoute + '/bootstrap.min.css' , 'utf-8');
+var flatIcon = fs.readFileSync(cssRoute + '/flaticon.css' , 'utf-8');
+var fontAwesome = fs.readFileSync(cssRoute + '/font-awesome.css' , 'utf-8');
+var style = fs.readFileSync(cssRoute + '/style.css' , 'utf-8');
+var revStyle = fs.readFileSync(cssRoute + '/rev-style.css' , 'utf-8');
+var themeColor = fs.readFileSync(cssRoute + '/theme-color/bootstrap.css' , 'utf-8');
 
 
 var styles = [
@@ -41,12 +41,11 @@ exports.printpdf1 = function (req, res) {
     var offer = req.body;
 
 
-    var renderedHtml =  appmodule.env.render(templateRoute + offer_template, {
+    var renderedHtml =  appmodule.env.render(templateRoute + offer_template_v1, {
         numberWithCommas: numberWithCommas,
         offer: offer,
         styles: styles
     });
-
     pdf.create(renderedHtml).toBuffer(function(err, buffer){
         if (err) console.log(err);
         else {
