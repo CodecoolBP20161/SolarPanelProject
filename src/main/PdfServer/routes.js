0f3cc1494ff63dc52a50b1f50be996981f3ff483
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-07-26 12:20:09 +0200

    set title and favicon

--- src/main/PdfServer/routes.js
+++ src/main/PdfServer/routes.js
@@ -2,7 +2,30 @@ var pdf = require('html-pdf');
 var appmodule = require('./server');
 var nunjucks = require("nunjucks");
 var path = require("path");
-var templateRoute = path.resolve(__dirname, 'templates/offer_template.html');
+var fs = require('fs');
+var templateRoute = path.resolve(__dirname, 'templates/');
+var cssRoute = path.resolve(__dirname, 'templates/assets/css/');
+var offer_template = 'offer_template.html';
+var offer_template_v1 = 'offer_template_v1.html';
+
+// Style files loaded
+var bootstrap = fs.readFileSync(cssRoute + 'bootstrap.min.css' , 'utf-8');
+var flatIcon = fs.readFileSync(cssRoute + 'flaticon.css' , 'utf-8');
+var fontAwesome = fs.readFileSync(cssRoute + 'font-awesome.css' , 'utf-8');
+var style = fs.readFileSync(cssRoute + 'style.css' , 'utf-8');
+var revStyle = fs.readFileSync(cssRoute + 'rev-style.css' , 'utf-8');
+var themeColor = fs.readFileSync(cssRoute + 'theme-color/bootstrap.css' , 'utf-8');
+
+
+var styles = [
+        bootstrap,
+        flatIcon,
+        fontAwesome,
+        style,
+        themeColor,
+        revStyle
+    ]
+    ;
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -18,9 +41,10 @@ exports.printpdf1 = function (req, res) {
     var offer = req.body;
 
 
-    var renderedHtml =  appmodule.env.render(templateRoute, {
+    var renderedHtml =  appmodule.env.render(templateRoute + offer_template, {
         numberWithCommas: numberWithCommas,
-        offer: offer
+        offer: offer,
+        styles: styles
     });
 
     pdf.create(renderedHtml).toBuffer(function(err, buffer){
