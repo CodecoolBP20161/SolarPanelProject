5aa0d46b826e813b347e0831723fca49ebf84d74
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-08-05 14:03:45 +0200

    implemented 'toJson()' function

--- src/main/java/com/codecool/models/Offer.java
+++ src/main/java/com/codecool/models/Offer.java
@@ -1,8 +1,11 @@
 package com.codecool.models;
 
+import com.codecool.models.enums.ItemTypeEnum;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -39,6 +42,27 @@ public class Offer {
         lineItems.remove(lineItem);
         nettoTotalPrice = nettoTotalPrice.subtract(lineItem.getTotal());
     }
+
+    public JSONObject toJson(){
+
+        JSONObject offerJson = new JSONObject();
+        JSONArray items = new JSONArray();
+        JSONArray services = new JSONArray();
+
+        offerJson.put("id", this.getId());
+        offerJson.put("isNetworkUpgradeNeeded", this.isNetworkUpgradeNeeded);
+        offerJson.put("taxRate", this.getTaxRate());
+        offerJson.put("netTotal", this.getNettoTotalPrice());
+        offerJson.put("grossTotal", this.getNettoTotalPrice().multiply(new BigDecimal(this.getTaxRate())));
+
+        for(LineItem item : lineItems){
+            if(item.getType() == ItemTypeEnum.Item) items.put(item.toJson());
+            else services.put(item.toJson());
+        }
+        return offerJson;
+    }
+
+
 }
 
 
