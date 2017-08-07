578dc4a55aa1c43c7ec9f8f85ab0203c9b35eb7b
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-08-07 13:04:02 +0200

    hardcoded offer json is used

--- src/main/java/com/codecool/services/PdfService.java
+++ src/main/java/com/codecool/services/PdfService.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.pdfbox.io.IOUtils;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -15,8 +16,9 @@ public class PdfService {
     private final String PDF_URL = "http://52.15.84.238:1350/api/getofferpdf";
 
     public File getPdf(Offer offer) throws UnirestException, IOException {
-        InputStream pdfInputStream = doPost(offer).getBody();
-        String offerId = String.valueOf(offer.getId());
+        InputStream pdfInputStream = doPost(offer).getRawBody();
+        System.out.println("InputStream: " + pdfInputStream);
+        String offerId = "123456";
 
         return  fileFromInputStream(pdfInputStream, offerId);
     }
@@ -30,10 +32,12 @@ public class PdfService {
     }
 
     private HttpResponse<InputStream> doPost(Offer offer) throws UnirestException {
+        String offerJson ="{\"id\": 123321, \"items\" : [ {\"name\": \"270W-os Amerisolar polikristályos napelem\",\"price\": 10000,\"quantity\": 10,\"subtotal\": 100000,\"description\": \"Legjobb termék\"}, {\"name\": \"270W-os Amerisolar polikristályos napelem\",\"price\": 10000,\"quantity\": 10,\"subtotal\": 100000,\"description\": \"Legjobb termék\"}, {\"name\": \"270W-os Amerisolar polikristályos napelem\",\"price\": 10000,\"quantity\": 10,\"subtotal\": 100000,\"description\": \"Legjobb termék\"}, {\"name\": \"270W-os Amerisolar polikristályos napelem\",\"price\": 10000,\"quantity\": 10,\"subtotal\": 100000,\"description\": \"Legjobb termék\"}], \"services\" : [ {\"name\": \"Service1\", \"price\": 10000, \"quantity\": 10, \"subtotal\": 100000, \"description\": \"Legjobb termék\"}, {\"name\": \"Service1\", \"price\": 10000, \"quantity\": 10, \"subtotal\": 100000, \"description\": \"Legjobb termék\"}, {\"name\": \"Service1\", \"price\": 10000, \"quantity\": 10, \"subtotal\": 100000, \"description\": \"Legjobb termék\"}, {\"name\": \"Service1\", \"price\": 10000, \"quantity\": 10, \"subtotal\": 100000, \"description\": \"Legjobb termék\"}, {\"name\": \"Service1\", \"price\": 10000, \"quantity\": 10, \"subtotal\": 100000, \"description\": \"Legjobb termék\"},],\"isNetworkUpgradeNeeded\": true,\"taxRate\": 27,\"netTotal\": 9999999,\"grossTotal\": 124343}";
+        JSONObject offerJsonObject = new JSONObject(offerJson);
         return Unirest.post(PDF_URL)
                       .header("accept", "application/pdf")
                       .header("Content-type", "application/json")
-                      .body(offer.toJson())
+                      .body(offerJsonObject)
                       .asBinary();
     }
 
