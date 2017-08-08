76ebdd2e07c842f2eabdee686a747d9fce99ae3c
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-08-08 14:49:55 +0200

    asd

--- src/main/resources/static/assets/js/offer-form/offer-form-1.js
+++ src/main/resources/static/assets/js/offer-form/offer-form-1.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     var phaseTwoInput = $('#phase2RadioInput');
     var alertSpan = $('#alert-phase');
     if (metricInput.val() == "0.0") metricInput.val("");
+    else (metricInput.val(numberWithCommas(metricInput.val())));
     submitted = false;
 
     // Changes the metric type accordingly
@@ -17,26 +18,44 @@ $(document).ready(function () {
     metricSelect.add(metricInput).add(phaseOneInput).add(phaseTwoInput)
         .on('ready change paste keyup', function () {
             metricInput.val(numberWithCommas(metricInput.val()));
-            if (isNetworkUpgradeNeeded(phaseOneInput, metricSelect, metricInput)) alertSpan.prop('hidden', false);
-            else alertSpan.prop('hidden', true);
-    });
+            isNetworkUpgradeNeeded(phaseOneInput, metricSelect, metricInput);
+        });
 
     $('#submit').on('click', function(){
         metricInput.val(metricInput.val().replace(".", ""));
-    })
+    });
 
 });
 
 var isNetworkUpgradeNeeded = function(phaseOneInput, metricSelect, metricInput){
-    // If phase is 1, and the selected metric is kWh and the input's value is bigger than 5, then upgrade is needed
-    var kWhIsTooHigh =  metricSelect.find('option:selected').val() == 'kWh' && metricInput.val() >= 5;
+    var alertSpan = $('#alert-phase');
+    if (phaseOneInput.val() == "1"){
+        var metric = metricSelect.find('option:selected').val();
+        var value = metricInput.val().replace(".", "");
 
-    // If phase is 1, and the selected metric is Ft and the input's value is bigger than 18000, then upgrade is needed
-    //TODO: Ask if it is really 18000, where it should warn about the phases
-    var HufIsTooHigh =  metricSelect.find('option:selected').val() == 'Ft' && metricInput.val().replace(".", "") >= 18000;
+        $.ajax({
+            url: "/ajanlat/network-upgrade",
+            type: 'POST',
+            async: true,
+            contentType: "application/json",
+            data: JSON.stringify({
+                "value": value,
+                "metric": metric
+            }),
+            success: function (needed) {
+                console.log("needed: " + needed);
+                if (needed == "true") {
+                    alertSpan.prop('hidden', false);
 
-    return phaseOneInput.is(':checked') && ( kWhIsTooHigh || HufIsTooHigh);
+                } else if (needed == "false") {
+                    alertSpan.prop('hidden', true);
+                }
+            }
+        });
+    }
+
 };
+
 var numberWithCommas = function (x) {
     return x.toString().replace(".", "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
