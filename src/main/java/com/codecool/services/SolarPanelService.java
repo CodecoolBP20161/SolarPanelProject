b20d17f59438d6d34171ab7d9867eb9be05d5ec7
gamte19 <gamte19@gmail.com>
2017-08-09 15:35:18 +0200

    callculate solar panel support structure

--- src/main/java/com/codecool/services/SolarPanelService.java
+++ src/main/java/com/codecool/services/SolarPanelService.java
@@ -12,10 +12,30 @@ public class SolarPanelService {
 
         int consumption = metric.equals("kWh") ? (int) value  :  (int) (((value * 12) / 37.5));
         int piece = consumption / solarPanelCapacity;
-
         while ((piece % 4) != 0) {
             piece++;
         }
+//:TODO beszéljük meg a napelem és tartók kalkulálását csak 4 napelem lehet egyszerre vagy lehet 2 esével is növelni?
+/*
+        int quantity = 0;
+
+        float value = consumptionForm.getValue();
+        String metric = consumptionForm.getMetric();
+        int consumption = metric.equals("kWh") ? (int) value  :  (int) (((value * 12) / 37.5));
+
+
+        return quantity;*/
         return piece;
+    }
+
+    public double callculateSolarPanelSupportStructure(double solarPanelQuantity) {
+        double quantity = 0;
+
+        if (solarPanelQuantity % 4 == 0) {
+            quantity = solarPanelQuantity / 4;
+        } else {
+            quantity = (solarPanelQuantity / 4) + 0.5;
+        }
+        return quantity;
     }
 }
