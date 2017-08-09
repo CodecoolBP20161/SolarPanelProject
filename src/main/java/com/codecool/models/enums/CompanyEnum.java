4203dc4f7ad5e22cf37046e8ad1e3780b36efc5b
gamte19 <gamte19@gmail.com>
2017-08-09 15:39:38 +0200

    Assign tax rate to firms

--- src/main/java/com/codecool/models/enums/CompanyEnum.java
+++ src/main/java/com/codecool/models/enums/CompanyEnum.java
@@ -2,16 +2,16 @@ package com.codecool.models.enums;
 
 public enum  CompanyEnum {
 //    TODO: change Cég1 to the real company név
-    Cég1,
+    TraditionalSolutions,
     StabilInvest,
     SolarProvider;
 
     private double taxRate;
 
     static {
-    Cég1.taxRate = 1.27;
-    StabilInvest.taxRate = 1.20;
-    SolarProvider.taxRate = 1.27;
+        TraditionalSolutions.taxRate = 1.27;
+        StabilInvest.taxRate = 1.20;
+        SolarProvider.taxRate = 1.27;
     }
 
     public double getTaxRate(){
