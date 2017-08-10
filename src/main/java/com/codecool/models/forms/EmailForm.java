e0a3c5741f3d68ad8f0c10b43150180588a591c5
gamte19 <gamte19@gmail.com>
2017-08-10 11:20:21 +0200

    remove unnecessary fields

--- src/main/java/com/codecool/models/forms/EmailForm.java
+++ src/main/java/com/codecool/models/forms/EmailForm.java
@@ -2,15 +2,31 @@ package com.codecool.models.forms;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class EmailForm {
-    private String firstName;
-    private String lastName;
+    @NotNull
+    @Min(value = 0, message = "The value must be bigger than 0")
+    @Max(value = 256, message = "Value must be less than 256")
+    private String name;
+
+    @NotNull
+    @Min(value = 0, message = "The value must be bigger than 0")
+    @Max(value = 256, message = "Value must be less than 256")
     private String emailAddress;
-    private String phoneNumber;
-    private String zipCode;
-    private String city;
-    private String address;
+
+    @NotNull
+    @Min(value = 0, message = "The value must be bigger than 0")
+    @Max(value = 256, message = "Value must be less than 256")
+    private String subject;
+
+    @NotNull
+    @Min(value = 0, message = "The value must be bigger than 0")
+    @Max(value = 2000, message = "Value must be less than 2000")
+    private String emailMessage;
 
 
 }
