dd2d3566f9d94d7f4bb417da0079c0da9597c5a2
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-08-12 14:11:48 +0200

    implemented

---
+++ src/main/resources/static/assets/js/admin/AJAX.js
@@ -0,0 +1,20 @@
+var doAJAX = function(URL, data, successFunction){
+    // var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    // var csrfToken = $("meta[name='_csrf']").attr("content");
+    // var headers = {};
+    // headers[csrfHeader] = csrfToken;
+
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        // headers: headers,
+        async: true,
+        contentType: "application/json",
+        data: data,
+        success: function (response) {
+            successFunction(response);
+        }
+    });
+
+};
+
