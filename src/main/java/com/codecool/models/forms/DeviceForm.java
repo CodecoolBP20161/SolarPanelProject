cd8d0f137c26c9b8c9598319594a70722c159c13
gamte19 <gamte19@gmail.com>
2017-09-19 22:02:06 +0200

    Add counstructor with argument

--- src/main/java/com/codecool/models/forms/DeviceForm.java
+++ src/main/java/com/codecool/models/forms/DeviceForm.java
@@ -7,6 +7,13 @@ public class DeviceForm {
     private String inverterId;
     private String panelId;
 
+    public DeviceForm(String inverterId, String panelId) {
+        this.inverterId = inverterId;
+        this.panelId = panelId;
+    }
+
+    public DeviceForm() {
+    }
 
     public boolean isValid(){return this.getInverterId() != null || this.getPanelId() != null; }
 }
